b7cea8b87c1416447a825e910ca1ed4038927758
Kim <Kim@DESKTOP-CORRDNM>
2017-08-10 21:50:08 +0900

    wd

---
+++ src/bbp/BulletinBoard.java
@@ -0,0 +1,83 @@
+package bbp;
+
+
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.table.DefaultTableModel;
+
+public class BulletinBoard extends JDialog{
+	//클래스말고 메서드로 (메서드 부르면 실행되게)
+
+	private JLabel title = new JLabel("제목 :");
+	private JButton buttonPurchase = new JButton("구매");
+	private JButton buttonBasket = new JButton("찜하기");
+	private JTable table = new JTable();
+	private JScrollPane scroll = new JScrollPane(table);	
+	private DefaultTableModel model = new DefaultTableModel();
+
+	private JPanel panelDescription = new JPanel();
+	private JButton buttonTest2 = new JButton("테스트용2");
+
+
+
+	public void compInit(){
+		this.setLayout(null);
+
+		this.title.setBounds(10,0,30,30);
+		this.add(title);
+
+		this.buttonPurchase.setBounds(220,400,80,30);
+		this.buttonBasket.setBounds(300,400,80,30);
+		this.add(buttonPurchase);
+		this.add(buttonBasket);
+
+
+
+		//			this.panelDescription.add(buttonTest2);
+		//			this.panelDescription.setBounds(100,200,100,100);
+
+
+
+	}
+
+	public void eventInit(){
+
+	}
+
+	public BulletinBoard(){
+		this.setSize(400, 500);
+		this.setLocationRelativeTo(null);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+		this.compInit();
+
+		this.setVisible(true);
+		this.setTitle("판매 책 정보");
+
+
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+			// If Nimbus is not available, you can set the GUI to another look and feel.
+		}
+		new BulletinBoard();
+	}
+
+}
+
