71ce05269521db25feded79f400d355e664d3e51
ITBANK403-01 <ITBANK403-01@ITBANK403-01-PC>
2017-08-10 19:33:09 +0900

    또 연습중입니다.

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,14 +2,15 @@ package Test;
 
 public class Test {
 	public static void main(String[] args) {
+		
 		System.out.println("ㅎㅇㅎㄹㄱㄴㅉㅇ");
 <<<<<<< HEAD
 		//지금 연습중입니다.
-	}
+	
 =======
 		
 		// 시험중 잘 되는지??
 		// 잘됩니다.
-	} 
->>>>>>> branch 'master' of https://github.com/teddypaul/BBP
-}
+	//잘 되고 있습니다.
+//>>>>>>> branch 'master' of https://github.com/teddypaul/BBP
+}}
