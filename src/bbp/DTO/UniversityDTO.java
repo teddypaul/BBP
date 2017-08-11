915249a55771e9364c763e31e123de6795d77f28
won <won@WON>
2017-08-11 13:36:54 +0900

    University DTO 업로드

---
+++ src/bbp/DTO/UniversityDTO.java
@@ -0,0 +1,19 @@
+package bbp.DTO;
+
+public class UniversityDTO {
+	private String universityname;
+	private String universityaddress;
+	
+	public String getUniversityname() {
+		return universityname;
+	}
+	public void setUniversityname(String universityname) {
+		this.universityname = universityname;
+	}
+	public String getUniversityaddress() {
+		return universityaddress;
+	}
+	public void setUniversityaddress(String universityaddress) {
+		this.universityaddress = universityaddress;
+	}
+}
