ca8b8b1146f085e8388a384a81aff9615af66bb6
won <won@WON>
2017-08-11 17:49:50 +0900

    학교 주소 디폴트 폴더에 인포트

--- src/bbp/DTO/UniversityDTO.java
+++ src/bbp/DTO/UniversityDTO.java
@@ -3,7 +3,14 @@ package bbp.DTO;
 public class UniversityDTO {
 	private String universityname; 		//학교이름
 	private String universityaddress;	//학교 주소
+	private String universityshop;
 	
+	public String getUniversityshop() {
+		return universityshop;
+	}
+	public void setUniversityshop(String universityshop) {
+		this.universityshop = universityshop;
+	}
 	public String getUniversityname() {
 		return universityname;
 	}
